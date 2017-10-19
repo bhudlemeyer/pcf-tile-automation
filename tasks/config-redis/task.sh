b6cbfd3e481c9a6ecaa4b9083b8dd5dc83d90bdb
Ryan Southard <eodchop@gmail.com>
2017-10-19 15:22:59 -0400

    test removed ~resources block. .bak is original

--- tasks/config-redis/task.sh
+++ tasks/config-redis/task.sh
@@ -71,10 +71,5 @@ PROPERTIES=$(cat <<-EOF
 EOF
 )
 
-RESOURCES=$(cat <<-EOF
-{
-}
-EOF
-)
 
-$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_NAME -p "$PROPERTIES" -pn "$NETWORK" -pr "$RESOURCES"
+$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_NAME -p "$PROPERTIES" -pn "$NETWORK" 
