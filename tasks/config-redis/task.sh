c3e80adfb09dc13eef8a7c27cf070221ae4096b3
Chris Mattingly <chris.mattingly@emc.com>
2017-10-27 14:37:27 -0400

    Update task.sh

--- tasks/config-redis/task.sh
+++ tasks/config-redis/task.sh
@@ -142,3 +142,36 @@ else
     done < <(echo "$will_disable")
   fi
 fi
+
+if [[ -z "$PREDELETE_ERRANDS_TO_DISABLE" ]] || [[ "$PREDELETE_ERRANDS_TO_DISABLE" == "none" ]]; then
+  echo "No pre-delete errands to disable"
+else
+  enabled_errands=$(
+  $CMD -t https://${OPS_MGR_HOST} -u $OPS_MGR_USR -p $OPS_MGR_PWD -k errands --product-name $PRODUCT_NAME |
+  tail -n+4 | head -n-1 | grep -v false | cut -d'|' -f2 | tr -d ' '
+  )
+  if [[ "$PREDELETE_ERRANDS_TO_DISABLE" == "all" ]]; then
+    errands_to_disable="${enabled_errands[@]}"
+  else
+    errands_to_disable=$(echo "$PREDELETE_ERRANDS_TO_DISABLE" | tr ',' '\n')
+  fi
+  
+  will_disable=$(for i in $enabled_errands; do
+      for j in $errands_to_disable; do
+        if [ $i == $j ]; then
+          echo $j
+        fi
+      done
+    done
+  )
+
+  if [ -z "$will_disable" ]; then
+    echo "All errands are already disable that were requested"
+  else
+    while read errand; do
+      echo -n Disabling $errand...
+      $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k set-errand-state --product-name $PRODUCT_NAME --errand-name $errand --pre-delete-state "disabled"
+      echo done
+    done < <(echo "$will_disable")
+  fi
+fi
