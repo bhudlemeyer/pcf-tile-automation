dd09019939b90b1c54ae483d959f8160cd8d9b8f
Chris Mattingly <chris.mattingly@emc.com>
2017-11-30 11:26:01 -0500

    errands changes

--- tasks/config-spring-cloud/task.sh
+++ tasks/config-spring-cloud/task.sh
@@ -46,3 +46,103 @@ EOF
 )
 
 $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_NAME -p "$PROPERTIES" -pn "$NETWORK" -pr "$RESOURCES"
+
+if [[ -z "$ERRANDS_TO_DISABLE" ]] || [[ "$ERRANDS_TO_DISABLE" == "none" ]]; then
+  echo "No post-deploy errands to disable"
+else
+  enabled_errands=$(
+  $CMD -t https://${OPS_MGR_HOST} -u $OPS_MGR_USR -p $OPS_MGR_PWD -k errands --product-name $PRODUCT_NAME |
+  tail -n+4 | head -n-1 | grep -v false | cut -d'|' -f2 | tr -d ' '
+  )
+  if [[ "$ERRANDS_TO_DISABLE" == "all" ]]; then
+    errands_to_disable="${enabled_errands[@]}"
+  else
+    errands_to_disable=$(echo "$ERRANDS_TO_DISABLE" | tr ',' '\n')
+  fi
+  
+  will_disable=$(for i in $enabled_errands; do
+      for j in $errands_to_disable; do
+        if [ $i == $j ]; then
+          echo $j
+        fi
+      done
+    done
+  )
+
+  if [ -z "$will_disable" ]; then
+    echo "All errands are already disable that were requested"
+  else
+    while read errand; do
+      echo -n Disabling $errand...
+      $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k set-errand-state --product-name $PRODUCT_NAME --errand-name $errand --post-deploy-state "disabled"
+      echo done
+    done < <(echo "$will_disable")
+  fi
+fi
+
+if [[ -z "$ERRANDS_TO_WHENCHANGED" ]] || [[ "$ERRANDS_TO_WHENCHANGED" == "none" ]]; then
+  echo "No post-deploy errands to set to when-changed"
+else
+  enabled_errands=$(
+  $CMD -t https://${OPS_MGR_HOST} -u $OPS_MGR_USR -p $OPS_MGR_PWD -k errands --product-name $PRODUCT_NAME |
+  tail -n+4 | head -n-1 | grep -v false | cut -d'|' -f2 | tr -d ' '
+  )
+  if [[ "$ERRANDS_TO_WHENCHANGED" == "all" ]]; then
+    errands_to_whenchanged="${enabled_errands[@]}"
+  else
+    errands_to_whenchanged=$(echo "$ERRANDS_TO_WHENCHANGED" | tr ',' '\n')
+  fi
+  
+  will_whenchanged=$(for i in $enabled_errands; do
+      for j in $errands_to_whenchanged; do
+        if [ $i == $j ]; then
+          echo $j
+        fi
+      done
+    done
+  )
+
+  if [ -z "$will_whenchanged" ]; then
+    echo "All errands are already set to when changed that were requested"
+  else
+    while read errand; do
+      echo -n Disabling $errand...
+      $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k set-errand-state --product-name $PRODUCT_NAME --errand-name $errand --post-deploy-state "when-changed"
+      echo done
+    done < <(echo "$will_whenchanged")
+  fi
+fi
+
+if [[ -z "$PREDELETE_ERRANDS_TO_DISABLE" ]] || [[ "$PREDELETE_ERRANDS_TO_DISABLE" == "none" ]]; then
+  echo "No pre-delete errands to disable"
+else
+  enabled_errands=$(
+  $CMD -t https://${OPS_MGR_HOST} -u $OPS_MGR_USR -p $OPS_MGR_PWD -k errands --product-name $PRODUCT_NAME |
+  tail -n+4 | head -n-1 | grep -v false | cut -d'|' -f2 | tr -d ' '
+  )
+  if [[ "$PREDELETE_ERRANDS_TO_DISABLE" == "all" ]]; then
+    errands_to_disable="${enabled_errands[@]}"
+  else
+    errands_to_disable=$(echo "$PREDELETE_ERRANDS_TO_DISABLE" | tr ',' '\n')
+  fi
+  
+  will_disable=$(for i in $enabled_errands; do
+      for j in $errands_to_disable; do
+        if [ $i == $j ]; then
+          echo $j
+        fi
+      done
+    done
+  )
+
+  if [ -z "$will_disable" ]; then
+    echo "All errands are already disable that were requested"
+  else
+    while read errand; do
+      echo -n Disabling $errand...
+      $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k set-errand-state --product-name $PRODUCT_NAME --errand-name $errand --pre-delete-state "disabled"
+      echo done
+    done < <(echo "$will_disable")
+  fi
+fi
+
